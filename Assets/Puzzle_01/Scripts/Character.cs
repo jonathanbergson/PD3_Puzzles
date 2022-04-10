5b3fc3046f576f38eb72f278ecd1360aa6db8097
Jonathan Bergson <contato@bergson.me>
2022-04-10 20:23:39 -0300

    chore: adicianado movimentação do personagem com a caixa

--- Assets/Puzzle_01/Scripts/Character.cs
+++ Assets/Puzzle_01/Scripts/Character.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const float Speed = 8.0f;
+    private const float RotationSpeed = 5.0f;
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+
+    public Animator animator;
+    private CharacterController _controller;
+
+    private Vector3 _moveDirection = Vector3.zero;
+    private bool _isMoving;
+
+    private void Awake()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            _isMoving = true;
+            _moveDirection = new Vector3(horizontal, 0, vertical);
+        }
+        else
+        {
+            _isMoving = false;
+        }
+
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        if (_isMoving)
+        {
+            animator.SetBool(IsMoving, true);
+            _controller.Move(_moveDirection * Speed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool(IsMoving, false);
+        }
+    }
+
+    private void HandleRotation()
+    {
+        if (_isMoving == false) return;
+
+        Vector3 lookDirection = _moveDirection;
+        lookDirection.y = 0;
+
+        Quaternion rotation = transform.rotation;
+        Quaternion toRotation = Quaternion.LookRotation(lookDirection);
+        transform.rotation = Quaternion.Slerp(rotation, toRotation, RotationSpeed * Time.deltaTime);
     }
 }
---
+++ Assets/Puzzle_01/Scripts/Force.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public class Force : MonoBehaviour
+{
+    private const float FloatingBoxHeight = 1.0f;
+    private const int RaycastDistance = 3;
+    private static readonly Color RaycastColor = Color.red;
+
+    private FixedJoint _joint;
+    private bool _useForce;
+    private bool _hasBoxGrabbed;
+
+    private Rigidbody _boxRigidbody;
+    private GameObject _boxGameObject;
+
+    private void Awake()
+    {
+        _joint = GetComponent<FixedJoint>();
+    }
+
+    private void Update()
+    {
+        HandleUseForce();
+        HandleRaycast();
+
+        Debug.DrawRay(transform.position, transform.forward * RaycastDistance, RaycastColor);
+    }
+
+    private void HandleUseForce()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _useForce = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _useForce = false;
+            DropBox();
+        }
+    }
+
+    private void HandleRaycast()
+    {
+        Vector3 position = transform.position;
+        Vector3 forward = transform.forward;
+
+        bool collided = Physics.Raycast(position, forward, out var hit, RaycastDistance);
+        if (collided && _useForce && _hasBoxGrabbed == false)
+        {
+            _boxRigidbody = hit.rigidbody;
+            _boxGameObject = hit.collider.gameObject;
+
+            GrabBox();
+        }
+    }
+
+    private void GrabBox()
+    {
+        _hasBoxGrabbed = true;
+
+        Vector3 position = transform.position + transform.forward * RaycastDistance;
+        Vector3 newPosition = new Vector3(position.x, FloatingBoxHeight, position.z);
+
+        _boxRigidbody.useGravity = false;
+        _boxGameObject.transform.rotation = Quaternion.identity;
+        _boxGameObject.transform.position = newPosition;
+        _joint.connectedBody = _boxRigidbody;
+    }
+
+    private void DropBox()
+    {
+        _hasBoxGrabbed = false;
+
+        if (_boxGameObject != null)
+        {
+            _boxRigidbody.useGravity = true;
+
+            _joint.connectedBody = null;
+            _boxGameObject = null;
+            _boxRigidbody = null;
+        }
+    }
+}
